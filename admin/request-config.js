3dbf762cf5ab82ddb4d98e065a010305196c0422
5Pages Builder <builder@5pages.dev>
2026-04-23 13:14:41 -0500

    Deploy site from 5Pages

---
+++ admin/request-config.js
@@ -0,0 +1,6 @@
+window.CLIENT_CMS_CONFIG = {
+  "siteId": "5fccb47f-5b90-46e9-8201-4776d5dd3ff8",
+  "requestApiBase": "http://localhost:4321/api/client-cms",
+  "requestToken": "add83685adaab9b0fc95ab6cdadf9e903f75693b246dcb5d",
+  "businessName": "Assignmate"
+};
